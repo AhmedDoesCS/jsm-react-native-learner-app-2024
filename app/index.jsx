e65981c2c8bab05186656345b1d9ae92824fa147
Ahmed Ahmed <ahmedqhasabu@gmail.com>
2025-02-08 02:56:24 +0200

    Project start

---
+++ app/index.jsx
@@ -0,0 +1,15 @@
+import {  Text, View } from 'react-native'
+import { Link } from 'expo-router'
+import React from 'react'
+
+const index = () => {
+  return (
+    <View className='flex items-center justify-center w-[100vw] h-[100vh]'>
+      <Text className='text-3xl'>Welcome!</Text>
+      <Link href="/profile">Go to profile.</Link>
+    </View>
+  )
+}
+
+export default index
+
