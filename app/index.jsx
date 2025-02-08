5a37ff7d35cb4da9ac9459f3c6db73ee23539162
Ahmed Ahmed <ahmedqhasabu@gmail.com>
2025-02-08 03:12:18 +0200

    Project Setup

--- app/index.jsx
+++ app/index.jsx
@@ -5,7 +5,7 @@ import React from 'react'
 const index = () => {
   return (
     <View className='flex items-center justify-center w-[100vw] h-[100vh]'>
-      <Text className='text-3xl'>Welcome!</Text>
+      <Text className='text-3xl font-pblack'>Welcome!</Text>
       <Link href="/profile">Go to profile.</Link>
     </View>
   )
