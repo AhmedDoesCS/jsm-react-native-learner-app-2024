3fca0344b2852d72dce512518dbdfd7fa51241ba
Ahmed Ahmed <ahmedqhasabu@gmail.com>
2025-02-08 03:28:12 +0200

    Tabs

--- app/index.jsx
+++ app/index.jsx
@@ -6,7 +6,7 @@ const index = () => {
   return (
     <View className='flex items-center justify-center w-[100vw] h-[100vh]'>
       <Text className='text-3xl font-pblack'>Welcome!</Text>
-      <Link href="/profile">Go to profile.</Link>
+      <Link href="/home">Go to profile.</Link>
     </View>
   )
 }
