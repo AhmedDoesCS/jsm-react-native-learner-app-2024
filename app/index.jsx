1155a7d46f5bfe6d72f2a3e64464e740bd60ab7b
Ahmed Ahmed <ahmedqhasabu@gmail.com>
2025-02-08 23:46:14 +0200

    Boarding page

--- app/index.jsx
+++ app/index.jsx
@@ -1,13 +1,22 @@
-import {  Text, View } from 'react-native'
+import {  Text, View, ScrollView, Image } from 'react-native'
 import { Link } from 'expo-router'
 import React from 'react'
+import {images} from '../constants'
 import { SafeAreaView } from 'react-native-safe-area-context'
 
 const index = () => {
   return (
     <View>
-      <SafeAreaView>
-        
+      <SafeAreaView className='bg-primary h-full'>
+        <ScrollView contentContainerStyle = {{height:'100%'}}>
+          <View className='w-full justify-center items-center h-full px-4'>
+            <Image
+            source={images.logo} 
+            className='w-[130px] h-[84px]'
+            resizeMode='contain'
+            />
+          </View>
+        </ScrollView>
       </SafeAreaView>
     </View>
   )
