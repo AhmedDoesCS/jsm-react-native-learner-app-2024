c9698952272c8b5bf2dbbef8acd59170110e9478
Ahmed Ahmed <ahmedqhasabu@gmail.com>
2025-02-09 16:01:15 +0200

    Backend

--- app/(auth)/sign-up.jsx
+++ app/(auth)/sign-up.jsx
@@ -1,13 +1,13 @@
-import { View, Text, ScrollView, Image } from 'react-native'
+import { View, Text, ScrollView, Image, Alert } from 'react-native'
 import {React, useState} from 'react'
 import { SafeAreaView } from 'react-native-safe-area-context'
 import { images } from '../../constants'
 import FormField from '@/components/FormField'
 import CustomButton from '@/components/CustomButton'
-import { Link } from 'expo-router'
+import { Link, router } from 'expo-router'
 import { createUser } from '../../lib/appwrite'
 
-const SignUp = () => {
+const SignUp = async () => {
   const [form, setForm] = useState({
     username:"",
     email: "",
@@ -16,8 +16,27 @@ const SignUp = () => {
 
   const [isSubmitting, setIsSubmitting] = useState(false)
 
-  const submit = () => {
-    createUser();
+  const submit = async () => {
+    if (!form.username || !form.email || !form.password) {
+      Alert.alert('Error', 'Please fill in all the fields')
+    }
+    setIsSubmitting(true);
+    
+    try {
+      const result = await createUser(
+        form.email,
+        form.password,
+        form.username
+      )
+
+      // set it to global state...
+      router.replace('/home')
+    } catch (error) {
+      Alert.alert('Error', error.message)
+    }
+    finally {
+      setIsSubmitting(false)
+    }
   }
 
   return (
