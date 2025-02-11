2a69c1f0a9209d71e4f25c3cd34978e29ac1e504
Ahmed Ahmed <ahmedqhasabu@gmail.com>
2025-02-11 11:55:17 +0200

    Backend

--- context/GlobalProvider.js
+++ context/GlobalProvider.js
@@ -1,4 +1,5 @@
 import { createContext, useContext, useState, useEffect } from "react";
+import { getCurrentUser } from '../lib/appwrite'
 
 const GlobalContext = createContext();
 export const useGlobalContext = () => useContext(GlobalContext);
@@ -6,17 +7,36 @@ export const useGlobalContext = () => useContext(GlobalContext);
 const GlobalProvider = ({ children }) =>  {
     const [isLoggedIn, setIsLoggedIn] = useState(false);
     const [user, setUser] = useState(null);
-    const [loading, setLoading] = useState(true);
+    const [isLoading, setIsLoading] = useState(true);
 
     useEffect(() => {
-        
+        getCurrentUser
+        .then((res) => {
+            if (res) {
+                setIsLoggedIn(true);
+                setUser(res)
+            } else {
+                setIsLoggedIn(false)
+                setUser(null)
+            }
+        })
+        .catch((error) => {
+            console.log(error);
+        })
+        .finally(() => {
+            setIsLoading(false)
+        })
     }, []);
 
 
     return (
         <GlobalContext.Provider
             value={{
-
+                isLoggedIn,
+                setIsLoggedIn,
+                user,
+                setUser,
+                isLoading
             }}
         >
             {children}
