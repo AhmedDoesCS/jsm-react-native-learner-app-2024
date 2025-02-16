b621ec2dc87fe009caa18dc0c77c456cff8d319a
Ahmed Ahmed <ahmedqhasabu@gmail.com>
2025-02-17 01:18:35 +0200

    Trending section (Home page)

--- app/(tabs)/home.jsx
+++ app/(tabs)/home.jsx
@@ -37,8 +37,10 @@ const Home = () => {
 
             <SearchInput />
 
-            <View>
-              
+            <View className='w-full flex-1 pt-5 pb-8'>
+              <Text className='text-gray-100 text-lg font-pregular mb-3'>
+                Latest videos
+              </Text>
             </View>
           </View>
         )}
