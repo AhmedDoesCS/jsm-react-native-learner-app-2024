23c41a23300a768cc9b7976fe480f6bd1c235357
Ahmed Ahmed <ahmedqhasabu@gmail.com>
2025-02-17 10:38:18 +0200

    Empty state (Home page)

--- components/EmptyState.jsx
+++ components/EmptyState.jsx
@@ -1,10 +1,22 @@
-import { View, Text } from 'react-native'
+import { View, Text, Image } from 'react-native'
 import React from 'react'
 
-const EmptyState = () => {
+import { images } from '../constants'
+
+const EmptyState = ({title, subtitle}) => {
   return (
-    <View>
-      <Text>EmptyState</Text>
+    <View className='w-full items-center justify-center'>
+        <Image
+            source={images.empty}
+            className='w-[270px] h-[215px]'
+            resizeMode='contain'
+         />
+         <Text className='font-pmedium text-sm text-gray-100'>
+            {title}
+        </Text>
+        <Text className='text-xl font-psemibold text-white mt-2'>
+             {subtitle}
+        </Text>
     </View>
   )
 }
