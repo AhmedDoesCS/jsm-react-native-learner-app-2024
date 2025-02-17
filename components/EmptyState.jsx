480c2a1902a7aeb46631f5c446ec5e4d732077e9
Ahmed Ahmed <ahmedqhasabu@gmail.com>
2025-02-17 10:43:59 +0200

    Empty state (Home page)

--- components/EmptyState.jsx
+++ components/EmptyState.jsx
@@ -2,6 +2,8 @@ import { View, Text, Image } from 'react-native'
 import React from 'react'
 
 import { images } from '../constants'
+import CustomButton from './CustomButton'
+import { router } from 'expo-router'
 
 const EmptyState = ({title, subtitle}) => {
   return (
@@ -11,12 +13,17 @@ const EmptyState = ({title, subtitle}) => {
             className='w-[270px] h-[215px]'
             resizeMode='contain'
          />
-         <Text className='font-pmedium text-sm text-gray-100'>
-            {title}
-        </Text>
         <Text className='text-xl font-psemibold text-white mt-2'>
-             {subtitle}
+             {title}
         </Text>
+         <Text className='font-pmedium text-sm text-gray-100'>
+            {subtitle}
+        </Text>
+        <CustomButton 
+            title="Create video"
+            handlePress={() => router.push('/create')}
+            containerStyles="w-[90vw] my-5"
+        />
     </View>
   )
 }
