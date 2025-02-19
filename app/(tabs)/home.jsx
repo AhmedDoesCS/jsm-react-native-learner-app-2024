a9c7b0e418ee67dcd181fe46d9c5b5050de4c0c0
Ahmed Ahmed <ahmedqhasabu@gmail.com>
2025-02-19 18:18:47 +0200

    Video card

--- app/(tabs)/home.jsx
+++ app/(tabs)/home.jsx
@@ -9,6 +9,7 @@ import EmptyState from '@/components/EmptyState'
 import { getAllPosts } from '@/lib/appwrite'
 
 import useAppwrite from '../../lib/useAppwrite'
+import VideoCard from '@/components/VideoCard'
 
 const Home = () => {
   const { data: posts, refetch} = useAppwrite(getAllPosts)
@@ -28,7 +29,7 @@ const Home = () => {
         data={posts}
         keyExtractor={(item) => item.$id} 
         renderItem={({ item }) => (
-          <Text className='text-3xl text-white'>{item.title}</Text>
+          <VideoCard video={item} />
           )}
         ListHeaderComponent={() => (
           <View className='my-6 px-4 space-y-6'>
